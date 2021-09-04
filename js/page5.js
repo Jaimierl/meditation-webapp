17ae8cb9a2f2545ddf8721cab292bc0fb3142534
Jaimierl <jaimieloney@gmail.com>
2021-09-04 13:03:39 -0400

    Cleaning up JS Page 5 page

--- js/page5.js
+++ js/page5.js
@@ -115,36 +115,13 @@ function renderVideo(){
 renderVideo();
 
 // let vid;
-// switch (chillaxResult,chillaxTypeResult){
-//   case '5','Guided':
+// switch (renderVideo){
+//   case '5':'Guided':
 //     vid = fiveGuided;
 //     break;
 // }
-// let onscreen =document.getElementById('playVideo');
-// onscreen.append = vid;
+// 
 
-
-// function renderVideo() {
-//   // Trying to render a message the playVideo div, Code not working.
-//   var age = prompt("How old are you?"),
-//       // Get a reference to the element we want to update
-//       el = document.getElementById('playVideo', message);
-
-//   // Check the age and set the message variable based on that
-//   if (age >= 18) {
-//       message = "Let's get started then!";
-//   } else {
-//       message = "You're under 18? Be careful out there....";
-//   }
-//   // Update the content of the element with the message
-//   el.innerHTML = message;
-// }
-
-// renderVideo();
-
-
-//  let testVideo = document.getElementById('chillaxVid');
-//  testVideo.innerHTML = video;
 
 
 // function onYouTubeIframeAPIReady() {
